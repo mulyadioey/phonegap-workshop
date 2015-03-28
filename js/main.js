50d986fc64e842736d334f033a62515527f2de8e
Mulyadi Oey <oey.mulyadi@gmail.com>
2015-03-28 17:44:52 +0700

    Set up a single page application.

--- js/main.js
+++ js/main.js
@@ -21,13 +21,24 @@ var app = {
         }
     },
 
+    renderHomeView: function() {
+        var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     initialize: function() {
 
         var self = this;
         this.store = new MemoryStore(function () {
+            self.renderHomeView();
             self.showAlert('Store Initialized', 'Info');
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
