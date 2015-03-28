bbfc320daffec0d7de8679996292bc4ae8fea518
Mulyadi Oey <oey.mulyadi@gmail.com>
2015-03-28 18:10:09 +0700

    Highlighting tapped or clicked UI elements.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,28 @@ var app = {
         }
     },
 
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
+
     initialize: function() {
 
         var self = this;
@@ -15,6 +37,7 @@ var app = {
             $('body').html(new HomeView(self.store).render().el);
             self.showAlert('Store Initialized', 'Info');
         });
+        this.registerEvents();
     }
 
 };
